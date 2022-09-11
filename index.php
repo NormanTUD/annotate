fed11f9cdd96e4951928df25acb2afd1bc13b283
root <root@v220220776251195133.powersrv.de>
2022-09-11 15:01:29 +0200

    title

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <head>
+	<title>Bildannotationstool</title>
 	<link rel="stylesheet" href="annotorious.min.css">
 	<script src="annotorious.min.js"></script>
 	<script src="jquery.min.js"></script>
