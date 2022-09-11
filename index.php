0ae3dda429161659209f1a25d9372e760acc2b6c
Norman Koch <kochnorman31@gmail.com>
2022-09-11 15:47:44 +0200

    show percentage

--- index.php
+++ index.php
@@ -38,7 +38,14 @@
 		}
 		$annotation_stat = get_number_of_annotated_imgs();
 ?>
-	<a href='tutorial.mp4' target="_blank">Video-Anleitung</a>, Anzahl annotierter Bilder: <?php print htmlentities($annotation_stat[0] ?? ""); ?>, Anzahl unannotierter Bilder: <?php print htmlentities($annotation_stat[1] ?? ""); ?><br>
+	<a href='tutorial.mp4' target="_blank">Video-Anleitung</a>, Anzahl annotierter Bilder: <?php print htmlentities($annotation_stat[0] ?? ""); ?>, Anzahl unannotierter Bilder: <?php print htmlentities($annotation_stat[1] ?? ""); ?>
+<?php
+		if($annotation_stat[1] != 0) {
+			$percent = sprintf("%0.2f", ($annotation_stat[0] / $annotation_stat[1]) * 100);
+			print " ($percent%)";
+		}
+?>
+	<br>
 	<div id="content">
 		<p><button onClick="refresh(this)">N&auml;chstes Bild</button><br></p>
 		<img id="image" src="images/<?php print $imgfile; ?>">
