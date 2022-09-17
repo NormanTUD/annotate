8690fb9af637dc689ebe273fb5148fb653c0a212
Norman Koch <norman.koch@tu-dresden.de>
2022-09-17 12:39:56 +0200

    fixed %

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@
 		<a href='tutorial.mp4' target="_blank">Video-Anleitung</a>, Anzahl annotierter Bilder: <?php print htmlentities($annotation_stat[0] ?? ""); ?>, Anzahl unannotierter Bilder: <?php print htmlentities($annotation_stat[1] ?? ""); ?>
 	<?php
 			if($annotation_stat[1] != 0) {
-				$percent = sprintf("%0.2f", ($annotation_stat[0] / $annotation_stat[1]) * 100);
+				$percent = sprintf("%0.2f", ($annotation_stat[0] / ($annotation_stat[0] + $annotation_stat[1])) * 100);
 				print " ($percent%)";
 			}
 	?>, <a href="overview.php">Übersicht über meine eigenen annotierten Bilder</a>, <a href="export_annotations.php">Annotationen exportieren</a>
