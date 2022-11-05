e4dbb64f56311a74dd13f53ea8054fa65d47d688
root <root@v220220776251195133.powersrv.de>
2022-11-05 16:47:48 +0100

    gitignore

--- main.js
+++ main.js
@@ -152,28 +152,22 @@ function create_annos () {
 	}
 }
 
- function open_tab(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+function open_tab(evt, cityName) {
+	var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
 }
-
-
 
 function toc () {
     var toc = "";
@@ -212,15 +206,13 @@ function toc () {
 };
 
 const toDataURL = url => fetch(url)
-  .then(response => response.blob())
-  .then(blob => new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onloadend = () => resolve(reader.result.split(',')[1])
-    reader.onerror = reject
-    reader.readAsDataURL(blob)
-  }))
-
-var xxx;
+	.then(response => response.blob())
+	.then(blob => new Promise((resolve, reject) => {
+		const reader = new FileReader()
+		reader.onloadend = () => resolve(reader.result.split(',')[1])
+		reader.onerror = reject
+		reader.readAsDataURL(blob)
+	}));
 
 function save_anno (annotation) {
 	// Do something
