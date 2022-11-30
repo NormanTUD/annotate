30ab72da14e12a45dcc9404d7dc0cc10444a38d3
Norman Koch <norman.koch@tu-dresden.de>
2022-11-30 18:45:40 +0100

    removed old code

--- export_annotations.php
+++ export_annotations.php
@@ -299,8 +299,9 @@
 
 			foreach ($filtered_imgs as $img) {
 				if(!array_key_exists("fn", $img)) {
-					error_log("fn not defined");
+					error_log("fn not defined:");
 					error_log(print_r($img, true));
+					continue;
 				}
 				if($max_files == 0 || $j < $max_files) {
 					$fn = $img["fn"];
