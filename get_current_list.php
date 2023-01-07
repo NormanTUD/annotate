6ff6af224150fbccc7681b5294567372b4aa229a
root <root@v220220776251195133.powersrv.de>
2023-01-07 15:01:52 +0100

    gitignore

--- get_current_list.php
+++ get_current_list.php
@@ -1,13 +1,20 @@
 <?php
 	include_once("functions.php");
 
-	$tags = get_current_tags();
-	$tags_as_array = [];
-	$html = "<ul style='list-style: conic-gradient'>";
-	foreach ($tags as $tag => $nr) {
-		$html .= "<li><a target='_blank' href='export_annotations.php?format=html&show_categories[]=".htmlentities(urlencode($tag))."'>$tag ($nr)</li>";
-	}
-	$html .= "</ul>";
+	$json_string = $GLOBALS["memcache"]->get("get_current_list");
 
-	print json_encode(array("html" => $html, "tags" => $tags));
+	if(!$json_string) {
+		$tags = get_current_tags();
+		$tags_as_array = [];
+		$html = "<ul style='list-style: conic-gradient'>";
+		foreach ($tags as $tag => $nr) {
+			$html .= "<li><a target='_blank' href='export_annotations.php?format=html&show_categories[]=".htmlentities(urlencode($tag))."'>$tag ($nr)</li>";
+		}
+		$html .= "</ul>";
+
+		$json_string = json_encode(array("html" => $html, "tags" => $tags));
+		$GLOBALS["memcache"]->set("get_current_list", $json_string, 0, 10);
+	}
+
+	print($json_string);
 ?>
