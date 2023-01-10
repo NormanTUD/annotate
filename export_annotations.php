e3165f861530a374da1049c85be0735e4fb95e50
root <root@v220220776251195133.powersrv.de>
2023-01-10 18:32:41 +0100

    allow params for omniopt

--- export_annotations.php
+++ export_annotations.php
@@ -960,6 +960,7 @@ python3 \$SCRIPT_DIR/train.py --cfg \"\$model\" --multi-scale --batch \$batchsiz
 	| awk '{print;print > \"/dev/stderr\"}' \\
 	| grep '/' \\
 	| grep -v Class \\
+	| sed -e \"s/.*G\s*//\" | egrep \"^[0-9]+\.[0-9]+\" \\
 	| sed -e 's/.*G\s*//' \\
 	| tail -n1 \\
 	| sed -e 's/\s*[0-9]*\s*[0-9]*:.*//' \\
