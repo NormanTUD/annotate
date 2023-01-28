8541e367b99cf632125b02b131a16f086258081b
Norman Koch <norman.koch@tu-dresden.de>
2023-01-28 16:51:29 +0100

    fix

--- get_current_annotations.php
+++ get_current_annotations.php
@@ -7,17 +7,17 @@
 	}
 
 	if(array_key_exists("source", $_GET)) {
-		$query = "select a.json from annotation a left join image i on a.image_id = i.id left join user u on u.id = a.user_id where i.filename = ".esc($_GET["source"])." and u.name  = ".esc($_COOKIE["annotate_userid"]).' and deleted = 0 order by a.modified desc limit 1';
+		$query = "select a.json from annotation a left join image i on a.image_id = i.id left join user u on u.id = a.user_id where i.filename = ".esc($_GET["source"])." and u.name  = ".esc($_COOKIE["annotate_userid"]).' and deleted = 0';
 
 		$res = rquery($query);
 
-		$json = [];
+		$jsons = [];
 
 		while ($row = mysqli_fetch_row($res)) {
-			$json = json_decode(stripcslashes($row[0]), TRUE)["full"];
+			$jsons[] = json_decode(json_decode(stripcslashes($row[0]), TRUE)["full"], true);
 		}
 
-		print $json;
+		print json_encode($jsons, TRUE);
 	} else {
 		print("[]");
 		exit(0);
