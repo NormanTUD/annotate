be414713ebee8de42a27a354bc188e5cb92d34ff
root <root@v220220776251195133.powersrv.de>
2023-02-04 14:47:18 +0100

    videos gehen

--- export_annotations.php
+++ export_annotations.php
@@ -52,12 +52,21 @@
 		$offset = intval(get_get("offset"));
 	}
 
+	$only_uncurated = 0;
+	if(get_get("only_uncurated")) {
+		$only_uncurated = intval(get_get("only_uncurated"));
+	}
+
 	$images = [];
 	
 	$annotated_image_ids_query = "select SQL_CALC_FOUND_ROWS i.filename, i.width, i.height, c.name, a.x_start, a.y_start, a.w, a.h, a.id from annotation a left join image i on i.id = a.image_id left join category c on c.id = a.category_id where i.id in (select id from image where id in (select image_id from annotation where deleted = 0 group by image_id)) and i.deleted = 0";
 
 	if(count($show_categories)) {
 		$annotated_image_ids_query .= " and c.name in (".esc($show_categories).")";
+	}
+
+	if($only_uncurated) {
+		$annotated_image_ids_query .= " and a.curated is null";
 	}
 
 	$annotated_image_ids_query .= " order by i.filename ";
