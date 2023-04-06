f7b289aaddfb6f5b7400f539bdf769347a8e54b5
Your Name <you@example.com>
2023-04-06 16:39:18 +0200

    fixes?

--- functions.php
+++ functions.php
@@ -145,14 +145,14 @@
 	}
 
 
-	function get_current_tags ($only_uncurated=0) {
+	function get_current_tags ($only_uncurated=0, $group_by_perception_hash=0) {
 		$annos = [];
 
-		$query = "select c.name, count(*) as anzahl from annotation a left join category c on c.id = a.category_id left join image i on a.image_id = i.id where i.deleted = 0 and a.deleted = 0 ";
+		$query = "select name, anzahl from (select c.name, count(*) as anzahl from annotation a left join category c on c.id = a.category_id left join image i on a.image_id = i.id where i.deleted = 0 and a.deleted = 0 ";
 		if($only_uncurated) {
 			$query .= " and a.curated is null ";
 		}
-		$query .= " group by c.id order by anzahl desc, c.name asc";
+		$query .= " group by c.id order by anzahl desc, c.name asc) a";
 		$res = rquery($query);
 
 		while ($row = mysqli_fetch_row($res)) {
