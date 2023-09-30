51ff384fe8934f65c2109b8532e5f7e23d15641d
Norman Koch <norman.koch@tu-dresden.de>
2023-09-30 20:36:13 +0200

    fix

--- upload_model.php
+++ upload_model.php
@@ -1,15 +1,13 @@
 <?php
-include("header.php");
-include_once("functions.php");
+	include("header.php");
+	include_once("functions.php");
 ?>
-
-<h1>Upload and Process PyTorch Model</h1>
-<form id="uploadForm" enctype="multipart/form-data" method="POST" action="upload_pt_model.php">
-    <input type="file" name="pytorch_model" accept=".pt">
-    <input type="text" name="model_name" placeholder="Model Name">
-    <input type="submit" value="Upload and Process Model">
-</form>
-
+	<h1>Upload and Process PyTorch Model</h1>
+	<form id="uploadForm" enctype="multipart/form-data" method="POST" action="upload_pt_model.php">
+		<input type="file" name="pytorch_model" accept=".pt">
+		<input type="text" name="model_name" placeholder="Model Name">
+		<input type="submit" value="Upload and Process Model">
+	</form>
 <?php
-include_once("footer.php");
+	include_once("footer.php");
 ?>
