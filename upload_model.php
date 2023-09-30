7e0477e7c9488ea54ca8f5f3eca5687dd87b544d
Norman Koch <norman.koch@tu-dresden.de>
2023-09-30 20:33:35 +0200

    fix

---
+++ upload_model.php
@@ -0,0 +1,15 @@
+<?php
+include("header.php");
+include_once("functions.php");
+?>
+
+<h1>Upload and Process PyTorch Model</h1>
+<form id="uploadForm" enctype="multipart/form-data" method="POST" action="upload_pt_model.php">
+    <input type="file" name="pytorch_model" accept=".pt">
+    <input type="text" name="model_name" placeholder="Model Name">
+    <input type="submit" value="Upload and Process Model">
+</form>
+
+<?php
+include_once("footer.php");
+?>
---
+++ upload_pt_model.php
@@ -0,0 +1,38 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		if (isset($_FILES['pytorch_model']) && isset($_POST['model_name'])) {
+			$modelName = $_POST['model_name'];
+			$pytorchModelName = $_FILES['pytorch_model']['name'];
+			$pytorchModelTmp = $_FILES['pytorch_model']['tmp_name'];
+
+			// Check if the uploaded file is a .pt file
+			if (pathinfo($pytorchModelName, PATHINFO_EXTENSION) === 'pt') {
+				// Define the command to run the Python script
+				$pythonCommand = 'bash pt_to_tfjs.sh '.$pytorchModelName;
+
+				// Execute the Python script
+				exec($pythonCommand, $output, $returnCode);
+
+				if ($returnCode === 0) {
+					// Loop through the generated files and insert them into the database
+					foreach (glob('yolov5/*.tfjs') as $tfjsFile) {
+						try {
+							insert_model_into_db($modelName, $tfjsFile, pathinfo($tfjsFile, PATHINFO_BASENAME));
+						} catch (\Throwable $e) {
+							echo "Error: <pre>$e</pre>";
+						}
+					}
+
+					// Display success message
+					success("Model Upload", "Model uploaded and processed successfully.");
+				} else {
+					echo "Error: Python script execution failed.";
+				}
+			} else {
+				echo "Error: Please upload a valid .pt file.";
+			}
+		} else {
+			echo "Error: Please provide both a model name and a .pt file to upload.";
+		}
+	}
+?>
