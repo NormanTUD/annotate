4f4f370c089ddc5f712a60f41bbb9ba9a625c66f
Norman Koch <norman.koch@tu-dresden.de>
2023-09-30 13:11:42 +0200

    better model check

--- has_model.php
+++ has_model.php
@@ -1,6 +1,24 @@
 <?php
+	include_once("functions.php");
+
 	if(file_exists("model.json")) {
-		print 1;
+		$json_str = file_get_contents("model.json");
+		try {
+			$json = json_decode($json_str, 1);
+
+			$paths = $json["weightsManifest"][0]["paths"];
+
+			foreach ($paths as $path) {
+				if(!file_exists($path)) {
+					print 0;
+					exit(0);
+				}
+			}
+			print 1;
+		} catch (\Throwable $e) {
+			print $e;
+			print "0";
+		}
 	} else {
 		print 0;
 	}
