966e8fe5b9af37e9d60524c68818d4555c993e73
Norman Koch <norman.koch@tu-dresden.de>
2023-10-01 20:53:43 +0200

    exporter fix

--- export_helper.php
+++ export_helper.php
@@ -894,7 +894,7 @@ done
 
 	function create_tmp_dir () {
 		$tmp_name = generateRandomString(20);
-		$tmp_dir = "tmp/$tmp_name";
+		$tmp_dir = "/tmp/$tmp_name";
 		while (is_dir($tmp_dir)) {
 			$tmp_name = generateRandomString(20);
 			$tmp_dir = "tmp/$tmp_name";
