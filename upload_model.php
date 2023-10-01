6b79fd9a9b4da83eac274e547e33f44c90476ecc
Norman Koch <norman.koch@tu-dresden.de>
2023-10-01 02:41:41 +0200

    model

--- upload_model.php
+++ upload_model.php
@@ -2,7 +2,8 @@
 	include("header.php");
 	include_once("functions.php");
 ?>
-	<h1>Upload and Process PyTorch Model</h1>
+	<h1>Upload yolov5-PyTorch Model</h1>
+	<p>Upload a .pt-file of your model. Name it, and you can then use it to annotate files.</p>
 	<form id="uploadForm" enctype="multipart/form-data" method="POST" action="upload_pt_model.php">
 		<input type="file" multiple name="pytorch_model" accept=".pt,.pb">
 		<input type="text" name="model_name" placeholder="Model Name">
