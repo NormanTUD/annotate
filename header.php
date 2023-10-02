623e08b4d82e1ad377e87cc05dff076af5cf3a2a
Norman Koch <norman.koch@tu-dresden.de>
2023-10-02 13:25:26 +0200

    moved

--- header.php
+++ header.php
@@ -5,14 +5,14 @@ include_once("functions.php");
 <!DOCTYPE html>
 <head>
 	<title>Bildannotationstool</title>
-	<link rel="stylesheet" href="annotorious.min.css">
+	<link rel="stylesheet" href="libs/annotorious.min.css">
 	<link rel="stylesheet" href="style.css">
-	<script src="plotly-latest.min.js"></script>
-	<script src="annotorious.min.js"></script>
-	<script src="jquery.min.js"></script>
+	<script src="libs/plotly-latest.min.js"></script>
+	<script src="libs/annotorious.min.js"></script>
+	<script src="libs/jquery.min.js"></script>
 	<script src="main.js"></script>
-	<script src='tf.js'></script>
-	<script src='tf-backend-wasm.min.js'></script>
+	<script src='libs/tf.js'></script>
+	<script src='libs/tf-backend-wasm.min.js'></script>
 	<!--<script src='phash.js'></script>-->
 	<script>
 		var labels = <?php include("labels.php"); ?>;
