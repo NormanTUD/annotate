5067ace5aecc3819d14edddab282aaa8494dcfb0
Norman <norman@optimalbliss.de>
2025-07-19 16:42:07 +0200

    fix

--- export_helper.php
+++ export_helper.php
@@ -26,7 +26,8 @@ for i in $(ls labels | sed -e "s#\.txt#.jpg#"); do
 done
 
 
-yolo task=detect mode=train data=dataset.yaml epochs='.$epochs.' imgsz=640';
+yolo task=detect mode=train data=dataset.yaml epochs='.$epochs.' imgsz=640
+';
 
 		file_put_contents("$tmp_dir/train", $train_bash);
 	}
