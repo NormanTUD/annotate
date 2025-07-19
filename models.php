59d967413d3fa672255cdf52a7fdeabfea86cfa1
Norman <norman@optimalbliss.de>
2025-07-19 16:04:14 +0200

    nicer

--- models.php
+++ models.php
@@ -37,7 +37,7 @@
 
 	<p>Upload a model.json and the according .bin files of your model. Name it, and you can then use it to annotate files.</p>
 
-	Code for export: <tt>yolo export model=best.pt format=tfjs</tt>, don't forget to also add the <tt>labels.json</tt>.
+	<p>Code for export: <tt>yolo export model=best.pt format=tfjs</tt>, don't forget to also add the <tt>labels.json</tt>.</p>
 
 	<form enctype="multipart/form-data" method="POST" action="upload_tfjs_model.php">
 		<input type="file" multiple name="tfjs_model[]" accept=".json,.bin">
