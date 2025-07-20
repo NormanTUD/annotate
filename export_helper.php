9091a73a9b39617d309d0ee86195de3531264d8e
n <norman.koch@tu-dresden.de>
2025-07-20 13:26:12 +0200

    onnx_graphsurgeon

--- export_helper.php
+++ export_helper.php
@@ -108,7 +108,7 @@ if [[ ! -d $VENV_PATH ]]; then
 	python3 -m venv $VENV_PATH
 	source $VENV_PATH/bin/activate
 	echo "Installing YoloV11"
-	pip3 install ultralytics onnx2tf tf_keras
+	pip3 install ultralytics onnx2tf tf_keras onnx_graphsurgeon
 else
 	echo "$VENV_PATH already exists"
 	source $VENV_PATH/bin/activate
