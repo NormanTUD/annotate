3cbc26d81e632165a69dc050165f8ea5f8d6a2f9
n <norman.koch@tu-dresden.de>
2025-11-19 02:59:06 +0100

    fix

--- get_model_file.php
+++ get_model_file.php
@@ -4,5 +4,10 @@
 	$uid = get_get("uid");
 	$filename = get_get("filename");
 
+	if ($filename === "model.json") {
+		header('Content-Type: application/json');
+		header('Content-Disposition: attachment; filename="model.json"');
+	}
+
 	print_model_file($uid, $filename);
 ?>
