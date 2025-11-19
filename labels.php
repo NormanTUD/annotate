3b40b81702259158fb15375457762f585ba7457d
n <norman.koch@tu-dresden.de>
2025-11-19 11:32:21 +0100

    removed labels.json as it is all from DB

--- labels.php
+++ labels.php
@@ -20,33 +20,29 @@
 	}
 
 	if(!$loaded_from_db) {
-		if (file_exists("labels.json")) {
-			print file_get_contents("labels.json");
-		} else {
-			include("export_helper.php");
+		include("export_helper.php");
 
-			$labels = [];
-			$categories = [];
+		$labels = [];
+		$categories = [];
 
-			$annotated_image_ids_query = 'SELECT name FROM category ORDER BY id';
-			$res = rquery($annotated_image_ids_query);
+		$annotated_image_ids_query = 'SELECT name FROM category ORDER BY id';
+		$res = rquery($annotated_image_ids_query);
 
-			while ($row = mysqli_fetch_row($res)) {
-				$category = $row[0];
-				if (!in_array($category, $categories)) {
-					$categories[] = $category;
-				}
+		while ($row = mysqli_fetch_row($res)) {
+			$category = $row[0];
+			if (!in_array($category, $categories)) {
+				$categories[] = $category;
 			}
-
-			$category_numbers = array();
-			$j = 0;
-			foreach ($categories as $i => $cat) {
-				$category_numbers[$cat] = $j;
-				$labels[] = $cat;
-				$j++;
-			}
-
-			print json_encode($labels);
 		}
+
+		$category_numbers = array();
+		$j = 0;
+		foreach ($categories as $i => $cat) {
+			$category_numbers[$cat] = $j;
+			$labels[] = $cat;
+			$j++;
+		}
+
+		print json_encode($labels);
 	}
 ?>
