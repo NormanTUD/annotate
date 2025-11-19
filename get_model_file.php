f69c40847c2d763eeb56b18fca509b34a0e8e68c
n <norman.koch@tu-dresden.de>
2025-11-19 03:01:36 +0100

    fix

--- get_model_file.php
+++ get_model_file.php
@@ -1,12 +1,29 @@
 <?php
-	include_once("functions.php");
+include_once("functions.php");
 
-	$uid = get_get("uid");
-	$filename = get_get("filename");
+$uid = get_get("uid");
+$filename = get_get("filename");
 
-	if ($filename === "model.json") {
-		header('Content-Type: application/json');
+if ($filename === "model.json") {
+	header('Content-Type: application/json');
+
+	$json = print_model_file($uid, $filename, true); // wir geben hier das JSON als String zurück
+
+	$data = json_decode($json, true);
+
+	if (isset($data['weightsManifest']) && is_array($data['weightsManifest'])) {
+		foreach ($data['weightsManifest'] as &$manifest) {
+			if (isset($manifest['paths']) && is_array($manifest['paths'])) {
+				foreach ($manifest['paths'] as $i => $path) {
+					// Ersetze jeden Pfad mit der URL
+					$manifest['paths'][$i] = "get_model_file.php?uid=" . urlencode($uid) . "&filename=" . urlencode($path);
+				}
+			}
+		}
 	}
 
+	echo json_encode($data);
+} else {
 	print_model_file($uid, $filename);
+}
 ?>
