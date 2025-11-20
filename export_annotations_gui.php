5fffc3f899faa9b9db77e30e707b4080eb0174f0
n <norman.koch@tu-dresden.de>
2025-11-20 23:56:08 +0100

    better msg

--- export_annotations_gui.php
+++ export_annotations_gui.php
@@ -31,107 +31,117 @@
 		}
 	</script>
 
-	<table id="export_settings" border=1>
-		<tr>
-			<th>Explanation</th>
-			<th style="display: none">Option</th>
-			<th>Value</th>
-		</tr>
+<?php
+	if(get_number_of_annotated_imgs()) {
+?>
+		<table id="export_settings" border=1>
+			<tr>
+				<th>Explanation</th>
+				<th style="display: none">Option</th>
+				<th>Value</th>
+			</tr>
 
-		<tr>
-			<td>Model</td>
-			<td style="display: none">model</td>
-			<td>
-				<select id="model" onchange="change_url()">
-					<option>yolo11n.yaml</option>
-					<option>yolo11n-cls.yaml</option>
-					<option>yolo11n-obb.yaml</option>
-					<option>yolo11n-pose.yaml</option>
-					<option>yolo11n-seg.yaml</option>
-					<option>yolo11s-cls.yaml</option>
-					<option>yolo11s-obb.yaml</option>
-					<option>yolo11s-pose.yaml</option>
-					<option>yolo11s-seg.yaml</option>
-					<option>yolo11s.yaml</option>
-					<option>yolo11m-cls.yaml</option>
-					<option>yolo11m-obb.yaml</option>
-					<option>yolo11m-pose.yaml</option>
-					<option>yolo11m-seg.yaml</option>
-					<option>yolo11m.yaml</option>
-					<option>yolo11l-cls.yaml</option>
-					<option>yolo11l-obb.yaml</option>
-					<option>yolo11l-pose.yaml</option>
-					<option>yolo11l-seg.yaml</option>
-					<option>yolo11l.yaml</option>
-					<option>yolo11x-cls.yaml</option>
-					<option>yolo11x-obb.yaml</option>
-					<option>yolo11x-pose.yaml</option>
-					<option>yolo11x-seg.yaml</option>
-					<option>yolo11x.yaml</option>
-				</select>
-			</td>
-		</tr>
+			<tr>
+				<td>Model</td>
+				<td style="display: none">model</td>
+				<td>
+					<select id="model" onchange="change_url()">
+						<option>yolo11n.yaml</option>
+						<option>yolo11n-cls.yaml</option>
+						<option>yolo11n-obb.yaml</option>
+						<option>yolo11n-pose.yaml</option>
+						<option>yolo11n-seg.yaml</option>
+						<option>yolo11s-cls.yaml</option>
+						<option>yolo11s-obb.yaml</option>
+						<option>yolo11s-pose.yaml</option>
+						<option>yolo11s-seg.yaml</option>
+						<option>yolo11s.yaml</option>
+						<option>yolo11m-cls.yaml</option>
+						<option>yolo11m-obb.yaml</option>
+						<option>yolo11m-pose.yaml</option>
+						<option>yolo11m-seg.yaml</option>
+						<option>yolo11m.yaml</option>
+						<option>yolo11l-cls.yaml</option>
+						<option>yolo11l-obb.yaml</option>
+						<option>yolo11l-pose.yaml</option>
+						<option>yolo11l-seg.yaml</option>
+						<option>yolo11l.yaml</option>
+						<option>yolo11x-cls.yaml</option>
+						<option>yolo11x-obb.yaml</option>
+						<option>yolo11x-pose.yaml</option>
+						<option>yolo11x-seg.yaml</option>
+						<option>yolo11x.yaml</option>
+					</select>
+				</td>
+			</tr>
 
-		<tr>
-			<td>Max number of files  (0 = no limit)</td>
-			<td style="display: none">max_files</td>
-			<td><input onchange="change_url()" type="number" value="0" /></td>
-		</tr>
+			<tr>
+				<td>Max number of files  (0 = no limit)</td>
+				<td style="display: none">max_files</td>
+				<td><input onchange="change_url()" type="number" value="0" /></td>
+			</tr>
 
-		<tr>
-			<td>Epochs</td>
-			<td style="display: none">epochs</td>
-			<td><input onchange="change_url()" type="number" value="2000" /></td>
-		</tr>
+			<tr>
+				<td>Epochs</td>
+				<td style="display: none">epochs</td>
+				<td><input onchange="change_url()" type="number" value="2000" /></td>
+			</tr>
 
-		<tr>
-			<td>Validation Split Size</td>
-			<td style="display: none">validation_split</td>
-			<td><input onchange="change_url()" type="number" value="0" /></td>
-		</tr>
+			<tr>
+				<td>Validation Split Size</td>
+				<td style="display: none">validation_split</td>
+				<td><input onchange="change_url()" type="number" value="0" /></td>
+			</tr>
 
-		<tr>
-			<td>Test Split Size</td>
-			<td style="display: none">test_split</td>
-			<td><input onchange="change_url()" type="number" value="0" /></td>
-		</tr>
+			<tr>
+				<td>Test Split Size</td>
+				<td style="display: none">test_split</td>
+				<td><input onchange="change_url()" type="number" value="0" /></td>
+			</tr>
 
-		<tr>
-			<td>Empty?</td>
-			<td style="display: none">empty</td>
-			<td><input onchange="change_url()" type="checkbox" /></td>
-		</tr>
+			<tr>
+				<td>Empty?</td>
+				<td style="display: none">empty</td>
+				<td><input onchange="change_url()" type="checkbox" /></td>
+			</tr>
 
-		<tr>
-			<td>Group by perception hash?</td>
-			<td style="display: none">group_by_perception_hash</td>
-			<td><input onchange="change_url()" type="checkbox" /></td>
-		</tr>
+			<tr>
+				<td>Group by perception hash?</td>
+				<td style="display: none">group_by_perception_hash</td>
+				<td><input onchange="change_url()" type="checkbox" /></td>
+			</tr>
 
-		<tr>
-			<td>Only curated?</td>
-			<td style="display: none">only_curated</td>
-			<td><input onchange="change_url()" type="checkbox" /></td>
-		</tr>
+			<tr>
+				<td>Only curated?</td>
+				<td style="display: none">only_curated</td>
+				<td><input onchange="change_url()" type="checkbox" /></td>
+			</tr>
 
-		<tr>
-			<td>Only uncurated?</td>
-			<td style="display: none">only_uncurated</td>
-			<td><input onchange="change_url()" type="checkbox" /></td>
-		</tr>
+			<tr>
+				<td>Only uncurated?</td>
+				<td style="display: none">only_uncurated</td>
+				<td><input onchange="change_url()" type="checkbox" /></td>
+			</tr>
 
-		<tr>
-			<td>Max perception hash truncation?</td>
-			<td style="display: none">max_truncation</td>
-			<td><input onchange="change_url()" type="number" value="100" /></td>
-		</tr>
-	</table>
+			<tr>
+				<td>Max perception hash truncation?</td>
+				<td style="display: none">max_truncation</td>
+				<td><input onchange="change_url()" type="number" value="100" /></td>
+			</tr>
+		</table>
 
-	<button><a id="link" style="color: white; display: none" href="">Download</a></button>
+		<button><a id="link" style="color: white; display: none" href="">Download</a></button>
 
-	<script>
-		change_url();
-	</script>
+		<script>
+			change_url();
+		</script>
+<?php
+	} else{
+?>
+		No files were annotated. For exporting, at least one image must be annotated.
+<?php
+	}
+?>
 <?php
 	include_once("footer.php");
 ?>
