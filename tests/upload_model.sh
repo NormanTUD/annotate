fd49aa6fd498ad9634a38fa7ba1f821cd0fcb5b5
n <norman.koch@tu-dresden.de>
2025-11-20 11:48:17 +0100

    fix

--- tests/upload_model.sh
+++ tests/upload_model.sh
@@ -1,12 +1,17 @@
-curl 'http://localhost:1112/upload_model.php' \
+#!/bin/bash
+
+HOST=$1
+PORT=$2
+
+curl "http://$HOST:$PORT/upload_model.php" \
   -X POST \
   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0' \
   -H 'Accept: */*' \
   -H 'Accept-Language: en-US,en;q=0.5' \
-  -H 'Referer: http://localhost:1112/models.php' \
-  -H 'Origin: http://localhost:1112' \
+  -H "Referer: http://$HOST:$PORT/models.php" \
+  -H "Origin: http://$HOST:$PORT" \
   -H 'DNT: 1' \
   -H 'Connection: keep-alive' \
   -H 'Cookie: language_cookie=de; PHPSESSID=eipc9q7sc1rg3ul754et4kfeet' \
   -F "model_name=my_test_model" \
-  -F "pt_model_file=@tests/last.pt;filename=last.pt"
+  -F "pt_model_file=@last.pt;filename=last.pt"
