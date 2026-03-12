53647c06c51342fc5051189d6650337c0971e8cb
n <norman.koch@tu-dresden.de>
2026-03-12 21:29:32 +0100

    fix

--- export_helper.php
+++ export_helper.php
@@ -582,7 +582,7 @@ fi';
 		return $number_of_rows;
 	}
 
-	function get_annotated_image_ids_query ($max_truncation=100, $show_categories=0, $only_uncurated=0, $format="ultralytics_yolov5", $limit=0, $items_per_page=0, $offset=0, $only_curated=0) {
+	function get_annotated_image_ids_query ($max_truncation=100, $show_categories=0, $only_uncurated=0, $format="ultralytics_yolov5", $limit=0, $items_per_page=0, $offset=0, $only_curated=0, $show_files=[]) {
 		$annotated_image_ids_query = "select SQL_CALC_FOUND_ROWS i.filename, i.width, i.height, c.name, a.x_start, a.y_start, a.w, a.h, a.id, left(i.perception_hash, $max_truncation) as truncated_perception_hash from annotation a left join image i on i.id = a.image_id left join category c on c.id = a.category_id where i.id in (select id from image where id in (select image_id from annotation where deleted = '0' group by image_id)) and i.deleted = 0 ";
 
 		if($show_categories && count($show_categories)) {
