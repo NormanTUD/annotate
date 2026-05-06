17eb3ded284fed01aca4709f734ba54d6c0508d9
Norman Koch <norman.koch@tu-dresden.de>
2026-05-06 10:12:37 +0200

    fix

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ var skipped_images = [];
 
 		'.ai-analyzing::before{' +
 		'content:"";' +
-		'position:absolute; top:-2px; left:0; right:0; bottom:0;' +
+		'position:absolute; top:0; left:0; right:0; bottom:2px;' +
 		'background:repeating-linear-gradient(0deg,rgba(255,255,255,0.04) 0,rgba(255,255,255,0.04) 2px,rgba(0,0,0,0) 4px);' +
 		'pointer-events:none;' +
 		'animation:noise 1.2s infinite;' +
@@ -159,7 +159,7 @@ var skipped_images = [];
 		'.ai-analyzing::after{' +
 		'content:"";' +
 		'position:absolute;' +
-		'top:-2px; left:0; width:150%; height:150%;' +
+		'top:0; left:0; width:150%; height:calc(100% - 2px);' +
 		'background:linear-gradient(90deg,rgba(0,255,150,0) 0%,rgba(0,255,150,0.35) 50%,rgba(0,255,150,0) 100%);' +
 		'pointer-events:none;' +
 		'transform-origin:center;' +
