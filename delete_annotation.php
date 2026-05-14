1a0c33fcbbda12fd55b8cded152c19f7f4f647c3
n <norman.koch@tu-dresden.de>
2026-05-14 10:55:29 +0200

    fix

--- delete_annotation.php
+++ delete_annotation.php
@@ -1,19 +1,43 @@
 <?php
-	include_once("functions.php");
+    include_once("functions.php");
 
-	if(array_key_exists("source", $_POST)) {
-		if(array_key_exists("id", $_POST)) {
-			$annotate_userid = $_COOKIE["annotate_userid"];
-			$user_id = get_or_create_user_id($annotate_userid);
+    if(array_key_exists("source", $_POST)) {
+        if(array_key_exists("id", $_POST)) {
+            $annotate_userid = $_COOKIE["annotate_userid"];
+            $user_id = get_or_create_user_id($annotate_userid);
 
-			flag_deleted($_POST["id"]);
+            $raw_id = $_POST["id"];
+            $affected = 0;
 
-			print("OK Deleted");
-		} else {
-			die("No ID given");
-		}
-	} else {
-		die("No source given");
-	}
+            // Try 1: exact match (as-is)
+            flag_deleted($raw_id);
+            $affected = mysqli_affected_rows($GLOBALS['dbh']);
 
+            // Try 2: strip leading '#'
+            if ($affected === 0 && strpos($raw_id, '#') === 0) {
+                $stripped = substr($raw_id, 1);
+                flag_deleted($stripped);
+                $affected = mysqli_affected_rows($GLOBALS['dbh']);
+            }
+
+            // Try 3: add leading '#'
+            if ($affected === 0 && strpos($raw_id, '#') !== 0) {
+                flag_deleted('#' . $raw_id);
+                $affected = mysqli_affected_rows($GLOBALS['dbh']);
+            }
+
+            // Try 4: maybe data-id is the numeric DB primary key
+            if ($affected === 0 && is_numeric($raw_id)) {
+                $query = "DELETE FROM annotation WHERE id = " . intval($raw_id);
+                rquery($query);
+                $affected = mysqli_affected_rows($GLOBALS['dbh']);
+            }
+
+            print("OK Deleted ($affected rows affected, raw_id=$raw_id)");
+        } else {
+            die("No ID given");
+        }
+    } else {
+        die("No source given");
+    }
 ?>
