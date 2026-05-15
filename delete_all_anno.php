fb842efaae5511bdce7f929bac7dfc9c04fd116e
n <norman.koch@tu-dresden.de>
2026-05-15 11:30:06 +0200

    fix

--- delete_all_anno.php
+++ delete_all_anno.php
@@ -2,11 +2,14 @@
 	include_once("functions.php");
 
 	if(array_key_exists("image", $_GET)) {
-		delete_all_annos_from_image(get_image_id($_GET["image"]));
-
-		print("OK Deleted");
+		$image_id = get_image_id($_GET["image"]);
+		if(preg_match("/^\d+$/", $image_id)) {
+			delete_all_annos_from_image($image_id);
+			print("OK Deleted");
+		} else {
+			die("Image not found");
+		}
 	} else {
 		die("No source given");
 	}
-
 ?>
